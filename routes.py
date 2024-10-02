ce92c757210e48724c2f5efed44a23b95b95974c
Carlos H Pinheiro <carloshenriquepinheirodasilva@gmail.com>
2024-10-02 18:37:42 -0300

    update `routes.py`

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from main import get_data, API_KEY
 
 
@@ -17,7 +17,23 @@ def clima():
 
     data = get_data(url, cidade, API_KEY)
 
-    return render_template(f"clima.html", cidade = data["cidade"], temperatura = data["temperatura"], clima = data["clima"], umidade = data["umidade"], vento = data["vento"])
+    return render_template(f"clima.html", cidade = data["cidade"], temperatura = data["temperatura"], clima = data["clima"], umidade = data["umidade"], vento = data["vento"], icon = data["icon"], pais = data["pais"])
+
+
+# Tratamento de erros
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('index'))
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return redirect(url_for('index'))
+
+
+@app.errorhandler(KeyError)
+def handle_key_error(e):
+    return redirect(url_for('index'))
 
 
 if __name__ == ("__main__"):
