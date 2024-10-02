6eefb4caa54393a7590b89350876454ccef0eb38
Carlos H Pinheiro <carloshenriquepinheirodasilva@gmail.com>
2024-10-02 15:08:25 -0300

    update `routes.py`

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from main import get_data, API_KEY
 
 
 app = Flask(__name__)
@@ -8,9 +9,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/clima")
+@app.route("/clima", methods=['GET'])
 def clima():
-    return render_template("clima.html")
+    cidade = request.args.get('cidade')
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric&lang=pt_br"
+
+    data = get_data(url, cidade, API_KEY)
+
+    return render_template(f"clima.html", cidade = data["cidade"], temperatura = data["temperatura"], clima = data["clima"], umidade = data["umidade"], vento = data["vento"])
 
 
 if __name__ == ("__main__"):
